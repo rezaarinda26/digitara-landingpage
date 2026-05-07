fcfd25b5941e250d79e92c39fcde23f0c8d9a8f9
rezaarinda26 <rezaarinda26@gmail.com>
2026-05-07 09:53:59 +0700

    fix: force storage path to /tmp for vercel

--- api/index.php
+++ api/index.php
@@ -6,4 +6,7 @@ require __DIR__ . '/../vendor/autoload.php';
 // Bootstrap Laravel and handle the request...
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
+// Ensure storage path is writable (Vercel only allows /tmp)
+$app->useStoragePath('/tmp');
+
 $app->handleRequest(Illuminate\Http\Request::capture());
